7b82385798db7d11953051027ac118d5b7431742
sth4nothing <892223922@qq.com>
2021-03-12 13:51:26 +0800

    [add]为网络请求头添加一些字段

--- demo.py
+++ demo.py
@@ -1,4 +1,3 @@
-from lgqm_tr.utils import load_cookies, save_cookies
 import logging
 import os
 import sys
@@ -19,32 +18,37 @@ tid = 7319
 download_image = True
 cookies_path = lgqm_tr.Settings.cookies_path
 sess = Session()
+sess.headers.update({
+    'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Safari/537.36 Edg/89.0.774.45',
+})
 if os.path.exists(cookies_path):
-    load_cookies(sess, cookies_path)
+    lgqm_tr.utils.load_cookies(sess, cookies_path)
 if download_image:
-    r = sess.get(lgqm_tr.Settings.api, params={
-                      'module': 'login',
-                      'type': 'login',
-                  }).json()
+    r = sess.get(lgqm_tr.Settings.api,
+                 params={
+                     'module': 'login',
+                     'type': 'login',
+                 }).json()
     if r['Message'].get('messageval') != 'login_succeed':
         # 登录
         r = sess.post(lgqm_tr.Settings.api,
-                    params={
-                        'module': 'login',
-                        'type': 'login',
-                    },
-                    data={
-                        'username': 'xxxxxxx@yy.com',
-                        'password': 'your_password',
-                        'loginsubmit': 'yes',
-                        'loginfield': 'auto',
-                        'questionid': 0,
-                        'answer': ''
-                    }).json()
+                      params={
+                          'module': 'login',
+                          'type': 'login',
+                      },
+                      data={
+                          'username': 'xxxxxxx@yy.com',
+                          'password': 'your_password',
+                          'loginsubmit': 'yes',
+                          'loginfield': 'auto',
+                          'questionid': 0,
+                          'answer': ''
+                      }).json()
         assert r['Message'].get('messageval') == 'login_succeed'
-        save_cookies(sess, cookies_path)
+        lgqm_tr.utils.save_cookies(sess, cookies_path)
 
-th = lgqm_tr.threads.crawl_thread(tid, sess)
+th = lgqm_tr.crawl.crawl_thread(tid, sess)
 
 thread_dir = Path('./data/', str(tid))
 
@@ -59,5 +63,10 @@ file_path.write_text('\n\n\n'.join(th.posts))
 for name, url in th.images.items():
     logging.info(f'download img {name} -> {url}')
     img_path = thread_dir.joinpath(name)
-    r = sess.get(url)
+    r = sess.get(url, headers={
+        'accept': 'image/*',
+        'sec-fetch-site': 'same-site',
+        'sec-fetch-mode': 'no-cors',
+        'sec-fetch-dest': 'image',
+    }, verify=False)
     img_path.write_bytes(r.content)
